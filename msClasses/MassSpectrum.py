259179f02655d708f19bade7a6d8b6225cfe3c74
gns <g.n.sivalingam@gmail.com>
2014-02-27 17:00:59 +0000

    Added commented versions of files. Class docstrings still need to be finished, banner as well

---
+++ msClasses/MassSpectrum.py
@@ -0,0 +1,241 @@
+import TwoDdata as tdd
+import Species, Gaussian
+from scipy import optimize
+import time
+import numpy as np
+import collections
+import lib.utils as utils
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+
+class MassSpectrum(tdd.TwoDdata):
+    """Class for storing and deconvoluting mass spectra
+
+    Inherits from TwoDdata:
+    def __init__(self)
+    def readFile(self,filename,x_range=0,grain=1)
+    def normalisationBpi(self)
+    def normalisationArea(self)
+    def setNormalisationType(self,type)
+    def smoothingSG(self,window_len=3,smoothes=2,poly_order=1)
+    def _normalisePreset(self)
+    def restoreRawYvals(self)
+    def getAxesWithoutNans(self)
+    def calculateWeightedMeanStandardDeviation(self)
+    def calculateAreaUnderCurve(self)
+    def _calculateGradient(self)
+    def findPeaks(self,limit=0)
+    def addPeak(self,mz)
+    def plot(self,ax,**kwargs)
+    def plotgPeaks(self,ax,labels=0,**kwargs)
+    """
+    def __init__(self):
+        tdd.TwoDdata.__init__(self)
+        self.species = collections.OrderedDict()
+        self.simulatedSpecies = collections.OrderedDict()
+        self.simulatedSpectrum = []
+    
+    def addSpecies(self,speciesObj,allowReplace=False):
+        """Add a Species() object to the MassSpectrum.species dictionary.
+        This allows it to be used in the fitting procedure.
+        allowReplace means adding a second species with the same name to overwrite existing
+        items in the dictionary.
+        """
+        name = speciesObj.name
+        if not name in self.species.keys():
+            self.species[name] = speciesObj
+        else:
+            if not allowReplace:
+                print "Species with this name already exists.\nOperation failed"
+            else:
+                print "Species with this name exists, overwriting"
+                self.species[name] = speciesObj
+        
+        
+    def leastSquaresOptimisation(self,speciesNames,oneFwhm=False):
+        """Deconvolute mass spectrum using non linear least squares.
+        Setting oneFwhm to true results in the same value for peak full width
+        half height being used for all species.
+        """
+        # setting up initial parameters
+        zs,p0 = [],[]
+        for name in speciesNames:
+            zs.append(self.species[name].charges)
+            p0 += self.species[name].getp0(oneFwhm)
+        
+        # using scipy optimize
+        def errorfunc(p,xvals,yvals,zs,oneFwhm):
+            return self.forLeastSquaresOptimisation(p,xvals,zs,oneFwhm) - yvals
+        
+        startTime = time.time()
+        p1, success = optimize.leastsq(errorfunc,p0[:],args=(self.xvals,self.yvals,zs,oneFwhm))
+        print "Optimisation took:", time.time()-startTime, "s"
+        
+        # storing the fit
+        for i,p in enumerate(self._splitPs(p1)):
+            tempSp = Species.Species(speciesNames[i])
+            if not oneFwhm:
+                peakFwhm = p[4]
+            else:
+                peakFwhm = oneFwhm
+            tempSp.setValsFromp1([p[0],p[1],p[2],p[3],peakFwhm])
+            tempSp.setCharges(zs[i])
+            self.simulatedSpecies[speciesNames[i]] = tempSp
+            if oneFwhm:
+                self.simulatedSpecies[speciesNames[i]].peakFwhm = p1[4]
+        self.simulatedSpectrum = self._simulateSpectrum(speciesNames, self.xvals)
+    
+    def _simulateSpectrum(self,speciesNames,xvals):
+        """Builds a simulated spectrum using the fitted parameters of given species names.
+        (uses the result of deconvolution)
+        """
+        for i,name in enumerate(speciesNames):
+            if not i: 
+                combined = self.simulatedSpecies[name].simulateSpecies(xvals)
+            else: 
+                combined += self.simulatedSpecies[name].simulateSpecies(xvals)
+        return combined
+    
+    def errorCalculations(self,xvals):
+        """Calculates and prints to console error statistics for comparing the simulated
+        spectrum to the original data.
+        + average error per data point
+        + average error per m/z unit
+        + RMSD (root mean square deviation)
+        """
+        dataBpi = self.yvals/self.yvals.max()*100
+        simBpi = self.simulatedSpectrum/self.simulatedSpectrum.max()*100
+        difference = np.abs(dataBpi-simBpi)
+        
+        avErrorPerDataPoint = np.sum(difference)/len(difference)
+        print "Average error per datapoint: %.2f %%" %avErrorPerDataPoint
+        
+        mzGap = np.average(np.diff(xvals))
+        avErrorPerMz = avErrorPerDataPoint/mzGap
+        print "Average error per $m/z$ unit: %.2f %%" %avErrorPerMz
+        
+        rmsd = np.sqrt(np.sum(dataBpi-simBpi)**2/len(dataBpi))
+        print "RMSD: %.2f" %rmsd
+    
+    def forLeastSquaresOptimisation(self,p0,xvals,zs,oneFwhm):
+        """Creates the simulated mass spectrum for MassSpectrum.leastSquaresOptimisation().
+        p0 - parameters to be used in simulation
+        zs - charges to be simulated
+        oneFwhm - if True use same FWHM for each species.
+        """
+        ps = self._splitPs(p0)
+        combined = np.zeros(len(xvals))
+        zGauss = Gaussian.Gaussian()
+        
+        for i,p in enumerate(ps):
+            zGauss.setParameters(p[1], p[2], p[3])
+            for j,z in enumerate(zs[i]):
+                centre = utils.get_mz(p[0], z)
+                amplitude = zGauss.calculateAmplitude(centre)
+                if oneFwhm:
+                    combined += utils.draw_peaks['hybrid'](xvals,amplitude,centre,p0[4])
+                else:
+                    combined += utils.draw_peaks['hybrid'](xvals,amplitude,centre,p[4])
+        return combined
+    
+    def _splitPs(self,pLong):
+        """Splits 1D list created by leastSquaresOptimisation into a multidimensional list.
+        One dimension per species.
+        """
+        #5 params per species
+        return [pLong[pos:pos + 5] for pos in xrange(0, len(pLong),5)]
+    
+            
+    def getgPeaksFromIds(self,ids):
+        """Generates a new gPeaks dictionary containing only the gPeaks
+        with the supplied ids.
+        gPeak format is gPeak[id] = [mz,intensity]
+        """
+        gPeaksOut = {}
+        for id in ids:
+            gPeaksOut[id] = self.gPeaks[id]
+        return gPeaksOut
+
+    # ================================================================
+    # Plotting functions
+
+    
+    def plotSimulatedSpectrum(self,ax,xvals,speciesNames=0,**kwargs):
+        """Plots just the simulated mass spectrum as generated by self.leastSquaresOptimisation(),
+        with a default color of red.
+        All matplotlib.pyplot.plot() arguments are allowed in **kwargs.
+        """
+        # TODO(gns) speciesNames is an uneeded argument, remove and bug test
+        # TODO(gns)
+        #ax = utils.checkAx(ax) # to add in later
+        if not 'color' in kwargs:
+            kwargs['color'] = 'r'
+        ax.plot(xvals,self.simulatedSpectrum,**kwargs)   
+    
+    def plotTheoreticalMzs(self,ax,name,absolute=False,mass=0,**kwargs):
+        """Used to draw the positions of calculated charge state peaks.
+        Can be used with a Species() object stored in self.species by entering a 'name'
+        or can be calculated directly from 'mass'
+        If a value for absolute is given, it is used as the y axis position for charge
+        state labels.
+        All matplotlib.pyplot.plot() arguments are allowed in **kwargs.
+        """
+        self.species[name].plotTheoreticalMzs(ax,self.xvals,self.yvals,
+                                              absolute,mass,**kwargs)
+
+                    
+    def plotAllComponents(self,ax,xvals,yvals,liftPercentage=10,speciesNames=0,colourList=0,traceLabels='mass',labelSize='small',**kwargs):
+        """Plot mass spectrum, each of the deconvoluted elements and the simulated spectrum
+        traceLabels can be 'mass' or 'name', with name the Species name is printed on the
+        right tail of the trace.
+        Species names allow you to change the order of the list
+        """
+        # TODO - if you work out why you want to be able to supply the axes, write it down here!
+        # colours
+        if not colourList:
+            colourList = utils.colourList 
+        colourList = colourList * 50
+        
+        # heights and widths
+        max = yvals.max()
+        lift = liftPercentage/100. *max
+        xleft = xvals.max() - (xvals[0].max()-xvals.min())*0.005 
+        
+        # plotting species
+        if type(speciesNames).__name__ == 'int':
+            speciesNames = self.simulatedSpecies.keys()
+        j = 0
+        for i,name in enumerate(speciesNames):
+            tempys = self.simulatedSpecies[name].simulateSpecies(xvals)
+            ax.plot(xvals,tempys+(j*lift+ lift/3.),color=colourList[j])
+            if traceLabels == 'mass':
+                ax.annotate('%.2f Da' %self.simulatedSpecies[name].mass,xy=(xleft,(j*lift+ lift/2.5)),size=labelSize,horizontalalignment='right')
+            elif traceLabels == 'name':
+                ax.annotate('%s' %name,xy=(xleft,(j*lift+ lift/2.5)),size=labelSize,horizontalalignment='right')
+            j += 1
+            
+        # plotting combined
+        ax.plot(xvals,self.simulatedSpectrum+(j*lift+ lift/3.),color='r',label='Simulated')
+            
+        j += 1
+        
+        # data
+        ax.plot(xvals,yvals+(j*lift+ lift/3.),color='k',label='Experimental')
+            
+        # plotting parameters
+        ax.set_yticks([])
+        ax.legend(loc='upper right',prop=FontProperties(size=utils.legendFontSize))
+        ax.set_xlabel('$m/z$')
+        ax.set_ylabel('Intensity')  
+        
+        
+    def writeCoordinatesToTxt(self,filename):
+        """Write the base mass spectrum data to file
+        Each line is in the format of mz\tintensity (tab separated).
+        """
+        file = open(filename,'w')
+        for x,y in zip(self.xvals,self.yvals):
+            print>>file, "%.3f\t%.3f" %(x,y)
+        file.close()
+
---
+++ msClasses/Species.py
@@ -0,0 +1,234 @@
+import numpy as np
+import lib.utils as utils
+import Gaussian
+import collections
+from scipy import integrate
+
+class Species():
+    '''Holds the data for a single species.
+    Separate objects should be created for before and
+    after (auto) simulation'''
+    
+    def __init__(self,name):
+        self.name = name
+        self.zGauss = Gaussian.Gaussian()
+        self.charges = []
+        self.mass = 0.0
+        self.calcMassError = 0.0
+        self.peakFwhm = 0.0
+
+
+    def plotTheoreticalMzs(self,ax,xvals,yvals,absolute=0,mass=0,**kwargs):
+        """Used to draw the positions of calculated charge state peaks.
+        If a value for absolute is given, it is used as the y axis position for charge
+        state labels.
+        All matplotlib.pyplot.plot() arguments are allowed in **kwargs.
+        """
+        if not mass:
+            mass = self.mass
+        oneDone = False
+        for i,z in enumerate(np.arange(100)+1):
+            mz = utils.get_mz(mass, z)
+            if xvals[0] < mz < xvals[-1]:
+                # the line
+                if not oneDone: ax.axvline(mz, label="%s - %.1f Da +/- %.1f" %(self.name,self.mass,self.calcMassError), **kwargs)
+                else: ax.axvline(mz, **kwargs)
+                oneDone = True
+                # annotation
+                if z/2 == float(z)/2: lift = .1 * yvals.max()
+                else: lift = .2 * yvals.max()    
+                if not 'color' in kwargs:
+                    color = 'blue'
+                else:
+                    color = kwargs['color']      
+                if not absolute:
+                    ax.text(mz,lift,'+%d' %z, color=color)
+                else:
+                    ax.text(mz,absolute,'+%d' %z, color=color)
+    
+    def getp0(self,oneFwhm=0):
+        """Return the list of initial parameters for leastSquaresOptimisation()
+        (in MassSpectrum()) from this Species() object.
+        """
+        if oneFwhm:    
+            return [self.mass,self.zGauss.amplitude,self.zGauss.centre,self.zGauss.fwhm,oneFwhm]
+        else:
+            return [self.mass,self.zGauss.amplitude,self.zGauss.centre,self.zGauss.fwhm,self.peakFwhm]
+
+   
+
+    def setValsFromp1(self,p1):
+        """After leastSquaresOptimisation() set the fitted parameters (p1)
+        to this Species() object.
+        """
+        self.mass = p1[0]
+        self.zGauss.amplitude = p1[1]
+        self.zGauss.centre = p1[2]
+        self.zGauss.fwhm = p1[3]
+        self.peakFwhm = p1[4]     
+
+
+    def setVals(self,mass,z_mu,z_amp,z_fwhh,p_fwhh=0):
+        """Set parameters for this object.
+        """
+        # TODO(gns) - This function doesn't seem necessary, try to remove
+        # Not even sure if it would work, setValsFromp1 should break if p1 only has 4 elements
+        if p_fwhh:
+            self.setValsFromp1(mass,z_mu,z_amp,z_fwhh,p_fwhh)
+        else:
+            self.setValsFromp1(mass,z_mu,z_amp,z_fwhh)
+    
+    def simulateSpecies(self,xvals,peakShape='hybrid'):
+        """Simulates a mass spectrum using the object's attributes
+        Valid peak shapes are: 'hybrid', 'gaussian' & 'lorentzian
+        if one_fwhh is to be used be sure to set self.peakFwhm before
+        calling this function.
+        """
+        combined = np.zeros(len(xvals),dtype='float')  
+        for z in self.charges:
+            centre = utils.get_mz(self.mass, z)
+            amplitude = self.zGauss.calculateAmplitude(centre)
+            combined += utils.draw_peaks[peakShape](xvals,amplitude,centre,self.peakFwhm)
+
+        return combined
+    
+    def getMz(self,z):
+        """Calculate the m/z value for a given charge state using the mass 
+        from self.mass
+        """
+        return utils.get_mz(self.mass, z)
+    
+    def calculateMassAndCharges(self,mzs):
+        """Calculate the mass of a molecular species using the given m/z
+        values.
+        Primarily used as a subfunction of self.calculateMass().
+        """
+        iarray = mzs[:]   
+        iarray.sort()
+        iarray.reverse()
+        charges = collections.OrderedDict()      
+        lowest = 10000000
+        lowest_z = 0
+        zs = xrange(1,101)
+        for z in zs:
+            charges[z] = []
+            for i,mz in enumerate(iarray):
+                charges[z].append(utils.get_mass(mz,z+i))
+        
+        for z in charges.keys():
+            sd = np.std(charges[z])
+            if sd < lowest:
+                lowest = sd
+                lowest_z = z
+        
+        # calculating error
+        total_error = []
+        for mass in charges[lowest_z]:
+            total_error.append(abs(np.average(charges[lowest_z])-mass))
+        average_error = np.average(total_error)
+        
+        return np.average(charges[lowest_z]), average_error, [lowest_z+i for i in xrange(len(mzs))]
+        
+    def calculateMass(self,mzs):
+        """Calculate the mass of a molecular species using the given m/z
+        values.
+        Returns mass and error, where error is the average absolute
+        difference between the given m/z values and the m/z from the
+        calculated mass.
+        """
+        mass,error,charges = self.calculateMassAndCharges(mzs)
+        return mass,error
+
+
+    def setSpecies(self,gPeaks,peakFwhm=10):
+        """Setup this Species() object using gPeaks (d[id]=[mz,intensity])
+        Calculates the mass and estimates parameters for the other
+        Species() parameters for later use as initial values for
+        leastSquaresOptimisation() (MassSpectrum()).
+        """
+        xvals,yvals = [],[]
+        for id in gPeaks:
+            xvals.append(gPeaks[id][0])
+            yvals.append(gPeaks[id][1])
+        self.mass, self.calcMassError = self.calculateMass(xvals)
+        
+        if len(xvals) > 2:
+            self.zGauss.optimiseParameters(xvals, yvals, setValues=1)
+        else:
+            self.zGauss.estimateParameters(xvals, yvals, setValues=1)
+        if self.charges == []: self.charges = self._estimateCharges()
+        self.peakFwhm = peakFwhm 
+        
+        
+    def setSpeciesGivenMass(self,mass,xvals,yvals,peakFwhm,zs):
+        """Similar to Species.setSpecies(). Except that instead of using
+        gPeaks, the Species() object is setup automatically when given
+        values for mass and the charges to be analysed.
+        """
+        self.mass = mass
+        pseudogPeaks = {}
+
+        for z in zs:
+            xval = utils.get_mz(self.mass, z)
+            i = utils.closest(xval, xvals)
+            yval = yvals[i]
+            pseudogPeaks[z] = [xval,yval]
+        self.setSpecies(pseudogPeaks, peakFwhm)
+            
+    
+    def setCharges(self,charges):
+        """For this molecular species, set the charges which
+        are detected in the mass spectrometer.
+        """
+        self.charges = charges
+    
+    def _estimateCharges(self,limit=1):
+        """Estimate charges to be simulated by fitting the charge state
+        Gaussian distribution.
+        Used as a subfunction for self.setSpecies()
+        Limit is given as a percentage of the total height of the Gaussian
+        and is used as the cutoff point for whether a charge state is to
+        be included or discarded.
+        """
+        # TODO(gns) - perhaps lower the limit for atropos at least
+        # probably the default value as well.
+        zs = np.arange(1,151)
+        charges = []
+        for z in zs:
+            xval = utils.get_mz(self.mass, z)
+            height = self.zGauss.calculateAmplitude(xval)
+            if height > self.zGauss.amplitude*(float(limit)/100):
+                charges.append(z)
+        print charges
+        return charges
+    
+    def getTotalArea(self,xvals):
+        """Calculate the area under the deconvoluted mass spectrum for just this
+        species using the trapezium method of integration.
+        """
+        return integrate.trapz(self.simulateSpecies(xvals), xvals)#, dx, axis)
+    
+    def getTotalIntensity(self,xvals,yvals):
+        """Using the supplied data, sum the intensity of the highest
+        point of the data at the m/z value for each charge state in
+        self.charges using self.mass as the mass.
+        """
+        '''Gets experimental intensity'''
+        intensity = 0
+        for charge in self.charges:
+            mz = utils.get_mz(self.mass, charge)
+            i = utils.closest(mz, xvals)
+            intensity += yvals[i]
+        return intensity
+        
+        
+    
+    def getPeakLimits(self,z,leftMultiplier=1.0,rightMultiplier=1.0):
+        """Returns lower and upper m/z limits for charge state peaks
+        using self.peakFwhm as the original value, and using multipliers
+        to alter the left and right limits independently.
+        """
+        mz = self.getMz(z)
+        left = mz - (self.peakFwhm/2)*leftMultiplier
+        right = mz + (self.peakFwhm/2)*rightMultiplier
+        return [left,right]
